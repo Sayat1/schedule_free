1d05be601eb48d370309cfa59be1b53bcd9d4589
sayat <sayt9360@gmail.com>
2025-05-06 11:05:17 +0900

    pdp 1.9.1

--- schedulefree/adamw_schedulefree.py
+++ schedulefree/adamw_schedulefree.py
@@ -54,9 +54,10 @@ class AdamWScheduleFree(torch.optim.Optimizer):
                  eps: float = 1e-8,
                  weight_decay: float = 0,
                  warmup_steps: int = 0,
+                 stochastic_rounding=True,
                  r: float = 0.0,
                  weight_lr_power: float = 2.0,
-                 foreach: Optional[bool] = hasattr(torch, "_foreach_mul_")
+                 foreach: Optional[bool] = False
                  ):
 
         defaults = dict(lr=lr, 
@@ -65,14 +66,71 @@ class AdamWScheduleFree(torch.optim.Optimizer):
                         r=r,
                         k=0,
                         warmup_steps=warmup_steps,
+                        lr_prev=-1.0,
                         train_mode=True,
                         weight_sum=0.0,
                         lr_max=-1.0,
                         weight_lr_power=weight_lr_power,
                         weight_decay=weight_decay,
+                        stochastic_rounding=stochastic_rounding,
                         foreach=foreach)
         super().__init__(params, defaults)
-    
+        self.fused_back_pass = False
+        self.try_hook_kohya_fbp()
+
+    def try_hook_kohya_fbp(self):
+        self.kohya_original_patch_adafactor_fused = None
+
+        try:
+            # Import and patching will fail if not Kohya.
+            import library.adafactor_fused
+
+            # Get the original method so we can restore it later.
+            self.kohya_original_patch_adafactor_fused = library.adafactor_fused.patch_adafactor_fused
+
+            # Define the override.
+            def prodigy_patch_adafactor_fused(optimizer):
+                unwrapped_optimiser = None
+                if hasattr(optimizer, "optimizer"):
+                    # If the optimiser is wrapped, forward the calls to the actual optimiser.
+                    def _step(self, *args, **kwargs):
+                        return self.optimizer.step(*args, **kwargs)
+
+                    def _step_param(self, *args, **kwargs):
+                        return self.optimizer.step_param(*args, **kwargs)
+
+                    optimizer.step = _step.__get__(optimizer)
+                    optimizer.step_param = _step_param.__get__(optimizer)
+                    unwrapped_optimiser = optimizer.optimizer
+                else:
+                    unwrapped_optimiser = optimizer
+               
+                print(f"[{self.__class__.__name__}] Kohya pipeline detected with fused backward pass. Gradient hook patch successful.")
+                library.adafactor_fused.patch_adafactor_fused = unwrapped_optimiser.kohya_original_patch_adafactor_fused # Restore the original method.
+
+                unwrapped_optimiser.fused_back_pass = True
+                unwrapped_optimiser.kohya_original_patch_adafactor_fused = None
+
+            # Patch the method.
+            library.adafactor_fused.patch_adafactor_fused = prodigy_patch_adafactor_fused
+        except:
+            pass
+
+    def try_unhook_kohya_fbp(self):
+        if self.kohya_original_patch_adafactor_fused is None:
+            return
+
+        try:
+            # Import and patching will fail if not Kohya.
+            import library.adafactor_fused
+
+            # User did not opt for fused backward pass, so remove our hook.
+            library.adafactor_fused.patch_adafactor_fused = self.kohya_original_patch_adafactor_fused
+        except:
+            pass
+
+        self.kohya_original_patch_adafactor_fused = None
+
     def eval(self):
         for group in self.param_groups:
             train_mode = group['train_mode']
@@ -97,6 +155,38 @@ class AdamWScheduleFree(torch.optim.Optimizer):
                         p.data.lerp_(end=state['z'].to(p.data.device), weight=1-beta1)
                 group['train_mode'] = True
 
+    # Implementation by Nerogar. From: https://github.com/pytorch/pytorch/issues/120376#issuecomment-1974828905
+    def copy_stochastic_(self, target, source):
+        # create a random 16 bit integer
+        result = torch.randint_like(
+            source,
+            dtype=torch.int32,
+            low=0,
+            high=(1 << 16),
+        )
+
+        # add the random number to the lower 16 bit of the mantissa
+        result.add_(source.view(dtype=torch.int32))
+
+        # mask off the lower 16 bit of the mantissa
+        result.bitwise_and_(-65536)  # -65536 = FFFF0000 as a signed int32
+
+        # copy the higher 16 bit into the target tensor
+        target.copy_(result.view(dtype=torch.float32))
+
+    def smart_copy(self, target, source, stochastic_rounding, smart_delete_source):
+        if target is source:
+            return
+
+        if stochastic_rounding and target.dtype == torch.bfloat16 and source.dtype == torch.float32:
+            self.copy_stochastic_(target, source)
+        else:
+            target.copy_(source)
+
+        if smart_delete_source:
+            del source
+
+    @torch.no_grad()
     def step(self, closure=None):
         """Performs a single optimization step.
 
@@ -104,6 +194,10 @@ class AdamWScheduleFree(torch.optim.Optimizer):
             closure (callable, optional): A closure that reevaluates the model
                 and returns the loss.
         """
+        self.try_unhook_kohya_fbp()
+
+        if self.fused_back_pass:
+            return
 
         loss = None
         if closure is not None:
@@ -123,18 +217,16 @@ class AdamWScheduleFree(torch.optim.Optimizer):
             else:
               sched = 1.0
             
-            bias_correction2 = 1 - beta2 ** (k+1)
+            bias_correction2 = 1
             lr = group['lr']*sched*math.sqrt(bias_correction2)
             
             lr_max = group['lr_max'] = max(lr, group['lr_max'])
-            
-            weight = ((k+1)**r) * (lr_max**weight_lr_power)
-            weight_sum = group['weight_sum'] = group['weight_sum'] + weight
+            if group['lr_prev'] == -1.0:
+                d_k = group['d_k'] = 1
+            else:
+                d_k = group['d_k'] =  (group['lr_prev'] / lr)
+            group['lr_prev'] = lr
 
-            try:
-                ckp1 = weight/weight_sum
-            except ZeroDivisionError:
-                ckp1 = 0
 
             if not group['train_mode']:
                 raise Exception("Not in train mode!")
@@ -143,8 +235,8 @@ class AdamWScheduleFree(torch.optim.Optimizer):
             
             for p in active_p:
                 if 'z' not in self.state[p]:
-                    self.state[p]['z'] = torch.clone(p.data)
-                    self.state[p]['exp_avg_sq'] = torch.zeros_like(p.data)
+                    self.state[p]['z'] = p.detach().clone(memory_format=torch.preserve_format)
+                    self.state[p]['exp_avg_sq'] = torch.zeros_like(p.grad, memory_format=torch.preserve_format).detach()
 
             if group['foreach'] and len(active_p) > 0:
                 y, grad, exp_avg_sq, z = zip(*[(p.data, 
@@ -175,31 +267,50 @@ class AdamWScheduleFree(torch.optim.Optimizer):
                 torch._foreach_sub_(z, grad, alpha=lr)
             else:
                 for p in active_p:
-                    y = p.data # Notation to match theory
-                    grad = p.grad.data
+                    z_state = state['z']
+                    stochastic = group['stochastic_rounding']
 
+                    y, z = (p.float(), z_state.float()) if stochastic else (p, z_state)
+                    grad = p.grad.to(dtype=torch.float32, copy=True)
+                    
+
+                    update = None
                     state = self.state[p]
+                    
+                    weight_sum = group['weight_sum']
 
-                    z = state['z']
+                    #update_second_moment
                     exp_avg_sq = state['exp_avg_sq']
+                    exp_avg_sq.mul_(beta2 * d_k * d_k).addcmul_(grad, grad, value=1-beta2)
+                    denom = exp_avg_sq.sqrt()
 
-                    exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1-beta2)
-                    denom = exp_avg_sq.sqrt().add_(eps)
+                    #update_
+                    update = grad.div_(denom.add_(eps))
+                    del denom
+                    
+                    if update is not None:
+                        #update_params
+                        weight = lr ** 2
+                        weight_sum = group['weight_sum'] + weight
+                        ckp1 = weight / weight_sum if weight_sum else 0
 
-                    # Reuse grad buffer for memory efficiency
-                    grad_normalized = grad.div_(denom)
+                        xy_step = 1 - beta1 * (1 - ckp1)
 
-                    # Weight decay calculated at y
-                    if decay != 0:
-                        grad_normalized.add_(y, alpha=decay)
+                        y.lerp_(end=z, weight=ckp1)
 
-                    # These operations update y in-place,
-                    # without computing x explicitly.
-                    y.lerp_(end=z, weight=ckp1)
-                    y.add_(grad_normalized, alpha=lr*(beta1*(1-ckp1)-1))
+                        if decay != 0:
+                            decay *= lr
+                            z.sub_(y, alpha=decay)
+                            y.sub_(y, alpha=decay * xy_step)
 
-                    # z step
-                    z.sub_(grad_normalized, alpha=lr)
+                        z.sub_(update, alpha=lr)
+                        y.sub_(update, alpha=lr * xy_step)
 
+                        self.smart_copy(p, y, stochastic, True)
+                        self.smart_copy(z_state, z, stochastic, True)
+
+                        del update
+
+            group['weight_sum'] = weight_sum
             group['k'] = k+1
         return loss
